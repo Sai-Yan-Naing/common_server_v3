31d63664ceef211bb0c8a7ca71eb28050616f9e7
sai-yan-naing <capital.saiyannaing@gmail.com>
2021-09-10 18:17:17 +0900

    add config example file

---
+++ web/config/all_example.php
@@ -0,0 +1,44 @@
+<?php
+define("PASS_KEY", "cee358b7cd218370be64e518934687be");
+
+define("ROOT", 'root');
+define("ROOT_PASS", "welcome");
+define("DSN", 'mysql:host=localhost:3306;dbname=japan_system_development_d');
+
+define("MAROOT", 'root');
+define("MAROOT_PASS", "welcome");
+define("MADSN", 'mysql:host=localhost:3307');
+
+define("IP", "203.137.92.252");
+
+// For email
+
+define("MAIL_HOST", "smtp.googlemail.com");
+define("MAIL_USER", "capital.saiyannaing@gmail.com");
+define("MAIL_PASS", "saiyannaing123!");
+define("FROM", "capital.saiyannaing@gmail.com");
+define("FROMNAME", "SAI YAN NAING");
+define("MAIL_PORT", 587);
+define("TO", "support@winserver.ne.jp");
+define("TONAME", "TESTER");
+define("MAIL_SERVER","http://ssl8.ethical-sai.tech");
+
+// define("STARTER_PLAN", 1);
+// define("COMMON_PLAN", 1);
+// define("PREMIUM_PLAN", 1);
+
+define('SWAF_PATH','C:/JP-Secure/SiteGuard Lite/logs/http/detect.log');
+define('ROOT_PATH','E:/webroot/LocalUser/');
+define('PHP_ROOT_PATH','C:/Program Files/PHP/');
+define('DEFAULT_PHP','v7.2.9-nts');
+define('DEFAULT_DOTNET','v4.0');
+
+const SQLSERVER_2016_DSN = "sqlsrv:Server=localhost;Database=master";
+const SQLSERVER_2016_USER = "sa";
+const SQLSERVER_2016_PASS = "welcome123";
+
+const SQLSERVER_2016_HOST_NAME = "mssql5.winserver.ne.jp";
+const SQLSERVER_2016_HOST_IP = "203.137.92.252";
+
+
+?>
