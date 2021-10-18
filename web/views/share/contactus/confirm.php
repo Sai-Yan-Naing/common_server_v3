ac3030c22619dabe67fe8b32743348c47519f06a
sai-yan-naing <capital.saiyannaing@gmail.com>
2021-10-18 17:13:11 +0900

    add mail template

--- web/views/share/contactus/confirm.php
+++ web/views/share/contactus/confirm.php
@@ -1,11 +1,30 @@
 <?php 
 require_once("views/share_config.php");
-	$to = $_POST['email'];
-	$toName = $_POST['name'];
-	$subject = "mail subject";
-	$body = $_POST['message'];
-	$webmailer->sendMail($to,$toName,$subject,$body);
-	header("location: /share/contactus?act=index");
-	die('admin');
+$to = $_POST['email'];
+$toName = $_POST['name'];
+$message = $_POST['message'];
+$phone = $_POST['phone'];
+$today = date("Y/m/d");
 
-?>
+$timestamp = date("Y/m/d :h:m:s");
+
+$subject ='=?UTF-8?B?'.base64_encode('ウィンサーバー').'?=';
+$body = file_get_contents('views/mailer/share/contactus/user.php');
+$body = str_replace('$message', $message, $body);
+$body = str_replace('$phone', $phone, $body);
+$body = str_replace('$email', $to, $body);
+$body = str_replace('$name', $toName, $body);
+$body = str_replace('$today', $today, $body);
+$webmailer->sendMail($to,$toName,$subject,$body,CC);
+
+$body = file_get_contents('views/mailer/share/contactus/mail.php');
+$body = str_replace('$message', $message, $body);
+$body = str_replace('$contractID', $webadminID, $body);
+$body = str_replace('$phone', $phone, $body);
+$body = str_replace('$email', CC, $body);
+$body = str_replace('$name', $toName, $body);
+$body = str_replace('$today', $timestamp, $body);
+
+$webmailer->sendMail($to,$toName,$subject,$body,CC);
+header("location: /share/contactus?act=index");
+die('admin');
