2539ff1c5c417f6dbcb96b0535851443b075ac64
TopStar <topstar51@outlook.com>
2022-06-30 20:33:50 +0900

    push

--- web/views/share/server/site/app_install/index.php
+++ web/views/share/server/site/app_install/index.php
@@ -13,53 +13,70 @@
                                 $limit = 10;  
                                 $table = 'app';
                                 require_once('views/pagination/start.php');
-                                $getAll= $commons->getAllRow("SELECT * FROM $table WHERE domain= ? and remove =0  ORDER BY id
+                                $getAll= $commons->getAllRow("SELECT * FROM $table WHERE domain= ? and remove = 0  ORDER BY id
                             OFFSET $start ROWS FETCH FIRST $limit ROWS ONLY",[$webdomain]);
                             ?>
                             <div class="tab-content">
                                 <div id="page-body" class="tab-pane active pr-3 pl-3"><br>
                                 <button class="btn btn-info btn-sm common_dialog" gourl="/share/server?setting=site&tab=app_install&act=new&webid=<?=$webid?>"  data-toggle="modal" data-target="#common_dialog"><span class="mr-2"><i class="fas fa-plus-square"></i></span>アプリケーション追加</button>
                                     <div class="mt-4 table-responsive">
-                                        <table class="table table-bordered">
-                                            <tr>
-                                                <th class="border-dark">サイト名</th>
-                                                <th class="border-dark">アプリケーション</th>
-                                                <th class="border-dark">バージョン</th>
-                                                <th class="border-dark">Document Root</th>
-                                                <th class="border-dark">URL</th>
-                                                <th class="border-dark">ユーザー名</th>
-                                                <th class="border-dark">パスワード</th>
-                                                <th class="border-dark">データベース</th>
-                                                <th class="border-dark">DBユーザー</th>
-                                                <th class="border-dark">DBパスワード</th>
-                                                <th class="font-weight-bold border-dark">操作</th>
-                                            </tr>
                                             <?php foreach($getAll as $app){ ?>
-                                            <tr>
-                                                <td class="border-dark"><?= $app['site_name'] ?></td>
-                                                <td class="border-dark"><?= $app['app_name'] ?></td>
-                                                <td class="border-dark"><?= $app['app_version'] ?></td>
-                                                <td class="border-dark"><a href="/share/server?setting=filemanager&tab=tab&act=index&webid=<?=$webid?>">/<?= $webuser ?>/<?= $app['root']?></a></td>
-                                                <td class="border-dark"><a href="<?= $app['url'] ?><?= ($app['app_name']=="WORDPRESS")? "/wp-admin/":(($app['app_version']=="eccube-4.1")?"/admin/login":"/admin")?>" target="_blank"><?= $app['url'] ?><?= ($app['app_name']=="WORDPRESS")? "/wp-admin/":(($app['app_version']=="eccube-4.1")?"/admin/login":"/admin")?></a></td>
-                                                <td class="border-dark"><?= $app['user_name'] ?></td>
-                                                <td class="border-dark"><div toggle='star' class="d-flex"><div class="col-sm-8">
+                                                <table class="table table-bordered">
+                                                    <thead>
+                                                        <tr>
+                                                            <th class="app-header"><i class="fa fa-caret-right mr-2" aria-hidden="true"></i> サイト名</th>
+                                                            <td><?= $app['site_name'] ?></td>
+                                                            <th>アプリケーション</th>
+                                                            <td><?= $app['app_name'] ?></td>
+                                                            <th>バージョン</th>
+                                                            <td><?= $app['app_version'] ?></td>
+                                                            <th>操作</th>
+                                                            <td>
+                                                                <a href="javascript:;" class="btn btn-outline-danger btn-sm common_dialog" gourl="/share/server?setting=site&tab=app_install&act=delete&act_id=<?=$app['id']?>&webid=<?=$webid?><?=$pagy?>"   data-toggle="modal" data-target="#common_dialog">削除</a>
+                                                            </td>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody style="display:none" class="app-body">
+                                                        <tr>
+                                                            <th colspan="2">ドキュメントルート</th>
+                                                            <th>URL</th>
+                                                            <th>ユーザー名</th>
+                                                            <th>パスワード</th>
+                                                            <th>データベース</th>
+                                                            <th>DBユーザー</th>
+                                                            <th>DBパスワード</th>
+                                                        </tr>
+                                                        <tr>
+                                                            <td colspan="2"><a href="/share/server?setting=filemanager&tab=tab&act=index&webid=<?=$webid?>">/<?= $webuser ?>/<?= $app['root']?></a></td>
+                                                            <td>
+                                                                <a href="<?= $app['url'] ?><?= ($app['app_name']=="WORDPRESS")? "/wp-admin/":(($app['app_version']=="eccube-4.1")?"/admin/login":"/admin")?>" target="_blank"><?= $app['url'] ?><?= ($app['app_name']=="WORDPRESS")? "/wp-admin/":(($app['app_version']=="eccube-4.1")?"/admin/login":"/admin")?></a>
+                                                            </td>
+                                                            <td>
+                                                                <?= $app['user_name'] ?>
+                                                            </td>
+                                                            <td>
+                                                                <div toggle='star' class="d-flex"><div class="col-sm-8">
                                                         <span class="d-none workbreakall"><?php echo htmlspecialchars($app['password'], ENT_QUOTES); ?></span><span class="star workbreakall" style='margin-top:5px'>********</span>
                                                         </div>
                                                         <div class="ml-auto col-sm-2">
-                <span class="fa fa-fw fa-eye fa-eye-slash tbtoggle-password"></span></div></td>
-                                                <td class="border-dark"><?= $app['db_name'] ?></td>
-                                                <td class="border-dark"><?= $app['db_user'] ?></td>
-                                                <td class="border-dark"><div toggle='star' class="d-flex"><div class="col-sm-8">
+                <span class="fa fa-fw fa-eye fa-eye-slash tbtoggle-password"></span></div>
+                                                            </td>
+                                                            <td>
+                                                                <?= $app['db_name'] ?>
+                                                            </td>
+                                                            <td><?= $app['db_user'] ?></td>
+                                                            <td>
+                                                                <div toggle='star' class="d-flex"><div class="col-sm-8">
                                                         <span class="d-none workbreakall"><?php echo htmlspecialchars($app['db_pass'], ENT_QUOTES); ?></span><span class="star workbreakall" style='margin-top:5px'>********</span>
                                                         </div>
                                                         <div class="ml-auto col-sm-2">
-                <span class="fa fa-fw fa-eye fa-eye-slash tbtoggle-password"></span></div></td>
-                                                    <td class="border-dark">
-                                                        <a href="javascript:;" class="btn btn-outline-danger btn-sm common_dialog" gourl="/share/server?setting=site&tab=app_install&act=delete&act_id=<?=$app['id']?>&webid=<?=$webid?><?=$pagy?>"   data-toggle="modal" data-target="#common_dialog">削除</a>
-                                                    </td>
-                                            </tr>
-                                            <?php } ?>
-                                        </table>
+                <span class="fa fa-fw fa-eye fa-eye-slash tbtoggle-password"></span></div>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                                <?php } ?>
+                                        
                                     </div>
                                 </div>
                             </div>
@@ -81,4 +98,20 @@
             </main>
         </div>
     </div> 
+
+<script>
+    $(document).on('click','.app-header',function(){
+        $this = $(this);
+        $(this).parent().parent().next(".app-body").slideToggle(function() {
+        if($this.parent().parent().next('.app-body').is(':hidden'))
+        {
+            $this.children('').removeClass('fa-caret-down')
+            $this.children('').addClass('fa-caret-right')
+        }else{
+            $this.children('').removeClass('fa-caret-right')
+            $this.children('').addClass('fa-caret-down')
+        }
+        });
+    })
+</script>
  <?php require_once("views/share/footer.php"); ?>
