71a92acd492826ea430c0cb44bf4e64b1e0705bf
TopStar <topstar51@outlook.com>
2022-12-09 08:26:01 +0900

    push

--- web/views/share/server/site/app_install/new.php
+++ web/views/share/server/site/app_install/new.php
@@ -93,7 +93,7 @@
             <div class="col-sm-6">
                 <label for="db-name" class="col-form-label">データベース名</label>
                 <div>
-                    <input type="text" class="form-control" id="db_name" data="db_name1" name="db_name" placeholder="1～64文字、半角英数字と_-" column="db_name" table="app" remark="checkappdb">
+                    <input type="text" class="form-control" id="db_name" data="db_name1" name="db_name" placeholder="1～64文字、半角英数字と_-" column="db_name" table="app" remark="">
                     <!-- <label for="db_name" id="db_name_ex_error" class="error"></label> -->
                 </div>
             </div>
@@ -101,7 +101,7 @@
                 <div class="col-sm-3"></div>
                 <label for="db_user" class="col-form-label">ユーザー名</label>
                 <div>
-                    <input type="text" class="form-control" id="db_user" name="db_user" placeholder="1～32文字、半角英数字と._-" column="db_user" table="app" remark="checkappdb">
+                    <input type="text" class="form-control" id="db_user" name="db_user" placeholder="1～32文字、半角英数字と._-" column="db_user" table="app" remark="">
                     <!-- <label for="db_user" id="db_user_ex_error" class="error"></label> -->
                 </div>
             </div>
